5783c3c1c8d29195fc8fce222dd00c22f88064ce
Nikhil Dhanda <nikhil4596@gmail.com>
2016-11-26 20:52:54 +0200

    Initial upload of all the project files

---
+++ js/app.js
@@ -0,0 +1,101 @@
+/**
+ * Created by nikhil on 11/26/2016.
+ */
+
+var genres = {
+
+    Pop : "132",
+    Rap : '116',
+    HipHop : '116',
+    Rock : '152',
+    Dance : '113',
+    RnB : '165',
+    Alternative : '85',
+    Electro : '106',
+    Folk : '466',
+    Reggae : '114',
+    Jazz : '129',
+
+};
+var idArr
+function tracksIdArr(arr) {
+    idArr = [];
+    for (var i = 0; i < arr.length; i++) {
+        idArr[i] = arr[i].id;
+    }
+    console.log(idArr);
+    load(idArr);
+
+
+}
+
+function loadSongs(id) {
+    var songsArr = [];
+    DZ.api("/radio/" + id + "/tracks", function (response) {
+        songsArr = response.data;
+        tracksIdArr(songsArr);
+        // console.log(songsArr)
+        });
+    // load(idArr);
+}
+
+function load(id) {
+    var ids = id;
+    // alert(id);
+    DZ.player.playTracks(ids);
+}
+
+function getGenreRadioId(str) {
+    var radioId;
+    var query1 = document.getElementById(str).value;
+    var genreId = genres[query1];
+    // alert(genreId);
+    radioId = "";
+    // DZ.api('/track/62724017', function(response){
+    DZ.api("/genre/" + genreId + "/radios", function(response){
+        radioId = response.data[0].id;
+        // alert(songId);
+        console.log("The response is ", response.data[0].id);
+        // console.log("The response is ", response.data[0].title);
+        loadSongs(radioId);
+
+    });
+    // alert(radioId);
+}
+// function getSongID(id) {
+//     var query = document.getElementById(id).value;
+//     // alert(idx);
+//     var songId = "12";
+//     // DZ.api('/track/62724017', function(response){
+//     DZ.api("/search/track?q='" + query + "'&index=0&limit=1", function(response){
+//         songId = response.data[0].id;
+//         // alert(songId);
+//         console.log("Title of the song is ", response.data[0].title);
+//         load(songId);
+//
+//     });
+// }
+
+function play() {
+    DZ.player.play();
+}
+
+function playOne() {
+    DZ.player.playTracks([134950958]);
+}
+DZ.init({
+    appId  : '215784',
+    channelUrl : 'http://localhost:8000',
+    player : {
+        container: 'player',
+        width : 800,
+        height : 200,
+        format : 'classic',
+        size : 'small',
+        autoplay : 'false',
+        playlist : true,
+        onload : function(){
+            // playOne();
+        }
+    }
+});
